7e32d2b3266be6e1a488f9519f993a5beabb59d1
taeho0888 <taeho0888@naver.com>
2023-04-08 23:30:42 +0900

    동기 요청으로 결과 받아오기 완료

--- page/views.py
+++ page/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_POST
+from .chatgpt import ChatBot
+import json
 
 
 def InputView(request):
@@ -8,13 +10,19 @@ def InputView(request):
 
 @require_POST
 def submit(request):
-    budget = request.POST.get('budget', '')
-    duration = request.POST.get('duration', '')
-    keyword = request.POST.get('keyword', '')
-    purpose = request.POST.get('purpose', '')
-    
-    # 콘솔에 출력
-    print(f'budget: {budget}, duration: {duration}, keyword: {keyword}, purpose: {purpose}')
-    
+    question = {}
+    for key, value in request.POST.items():
+        if key != 'csrfmiddlewaretoken':
+            question[key] = value  
+
+    gpt = ChatBot()
+    result = gpt.ask_question(question=question)
+    # context = {'result': '{\n    "일차": {\n        "09:00": {\n            "장소": "경주 첨성대",\n            "활동": "아날로그 카메라로 첨성대와 경주시내 사진 촬영",\n            "예상경비": "무료"\n        },\n        "11:00": {\n            "장소": "안압지",\n            "활동": "아날로그 카메라로 안압지와 자연 경관 촬영",\n            "예상경비": "무료"\n        },\n        "12:30": {\n            "장소": "소풍감귤농원",\n            "활동": "필름 카메라로 소풍감귤농원에서 휴식과 촬영",\n            "예상경비": "비용당 5,000원정도"\n        },\n        "16:00": {\n            "장소": "거제도",\n            "활동": "바다와 자연 경관 감상",\n            "예상경비": "인당 25만원"\n        },\n        "20:00": {\n            "장소": "거제도 숙박시설",\n            "활동": "휴식",\n            "예상경비": "인당 20만원"\n        }\n    }\n}'}
+
+
+    # result = json.loads(context['result'])
+
+    context = { 'result' : result }
+    # print(context)
     # 입력 정보 확인을 위해 입력 페이지로 이동
-    return render(request, 'input.html')
+    return render(request, 'result.html', context)
