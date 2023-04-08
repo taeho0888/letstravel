52d853b878a3d528d51887e1c26b2d9b784f3429
taeho0888 <taeho0888@naver.com>
2023-04-08 20:45:15 +0900

    post 요청 보내기 성공

--- page/urls.py
+++ page/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('input', InputView, name='InputView'),
+    path('submit', submit, name="submit"),
 ]
--- page/views.py
+++ page/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_POST
+
 
 def InputView(request):
+    return render(request, 'input.html')
+
+
+@require_POST
+def submit(request):
+    budget = request.POST.get('budget', '')
+    duration = request.POST.get('duration', '')
+    keyword = request.POST.get('keyword', '')
+    purpose = request.POST.get('purpose', '')
+    
+    # 콘솔에 출력
+    print(f'budget: {budget}, duration: {duration}, keyword: {keyword}, purpose: {purpose}')
+    
+    # 입력 정보 확인을 위해 입력 페이지로 이동
     return render(request, 'input.html')
